5d0fb96c18f054d58511b9b2ebf7dc0cdf5b17fc
Arnold Narterh <arnie.narterh@gmail.com>
2026-03-07 18:06:09 +0000

    assignment submission

--- src/app.js
+++ src/app.js
@@ -1,4 +1,8 @@
 import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+
+dotenv.config();
 import authRoutes from "./routes/authRoutes.js";
 import taskRoutes from "./routes/taskRoutes.js";
 
@@ -8,5 +12,15 @@ app.use(express.json());
 
 app.use("/api/auth", authRoutes);
 app.use("/api/tasks", taskRoutes);
+
+//connecting the database
+mongoose.connect(process.env.MONGO_URI)
+.then(() => {
+  console.log("Connected to MongoDB");
+  app.listen(process.env.PORT, () => {
+    console.log(`Server running on port ${process.env.PORT}`);
+  })
+})
+.catch(err => console.log(err));
 
 export default app;
--- src/middleware/authMiddleware.js
+++ src/middleware/authMiddleware.js
@@ -10,7 +10,34 @@ import User from "../models/User.js";
 // 6. If invalid → return 401
 
 const authMiddleware = async (req, res, next) => {
-  //  implement here
+  try {
+    //to extract the token from the header 
+    const authHeader = req.headers.authorization;
+
+    if (!authHeader || !authHeader.startsWith("Bearer ")){
+      return res.status(401).json({message: "No token provided"});
+    }
+    const token = authHeader.split(" ")[1];
+
+    //to verify the token
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+
+    //to find the user 
+    const user = await User.findById(decoded.id).select("-password");
+
+    if (!user) {
+      return res.status(401).json({message: "User not found"});
+    }
+
+    //attach user to request 
+    req.user = user;
+
+    //continue 
+    next();
+  }
+  catch (error) {
+    return res.status(401).json({message: "Invalid token"});
+  }
 };
 
 export default authMiddleware;
